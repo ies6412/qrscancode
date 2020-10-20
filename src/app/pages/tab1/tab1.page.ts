3e709944efd1e9a9a85ba1bbecfcb9d9f711ad4f
ies6412 <63573807+ies6412@users.noreply.github.com>
2020-10-19 20:35:34 -0500

    se puede ver en el dispositivo fisico

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,45 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+
+
+  swideroption: {
+    allowSlidePrev: false,
+    allowSlideNext: false
+   };
+
+  ionViewWillEnter(){
+
+    this.Scanner();
+  }
+  ionViewDidEnter(){
+
+    console.log('didenter');
+  }
+
+  ionViewDidLeave(){
+
+    console.log('Leaveenter');
+  }
+  ionViewWillLeave(){
+
+    console.log('ha salido');
+  }
+
+
+  constructor(private barcodeScanner: BarcodeScanner) {
+
+
+  }
+
+  Scanner(){
+
+    this.barcodeScanner.scan().then(barcodeData => {
+    console.log('Barcode data', barcodeData);
+   }).catch(err => {
+       console.log('Error', err);
+   });
+
+  }
 
 }
