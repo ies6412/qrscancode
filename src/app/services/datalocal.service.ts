da13d73e4d041dc50b9c1a9a828cd9ee13cbc3ea
ies6412 <63573807+ies6412@users.noreply.github.com>
2020-10-20 14:09:55 -0500

    se daño

--- src/app/services/datalocal.service.ts
+++ src/app/services/datalocal.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
 import { Registro } from '../models/registro.models';
 
 @Injectable({
@@ -7,15 +9,39 @@ import { Registro } from '../models/registro.models';
 export class DatalocalService {
   guardados: Registro[] = [];
 
-  constructor() { }
+  constructor(private storage: Storage,
+              private NavCtrl: NavController) {
 
-  GuardarRegistro(format: string, text: string){
+    this.CargarStorage();
+    // this.storage.get('registro')
+    // .then(registros => {
+    //   this.guardados = registros || [];
+    // });
+  }
+  async CargarStorage(){
+    this.guardados = await this.storage.get('registro') || [];
+  }
 
+  async GuardarRegistro(format: string, text: string){
+
+    await this.CargarStorage();
     const guardado = new Registro(format, text);
     this.guardados.unshift(guardado);
+
     console.log('lo que se esta guardando', this.guardados);
+    this.storage.set('registro', this.guardados);
 
 
+
+
+  }
+  abrirRegistro(registro: Registro){
+    this.NavCtrl.navigateForward('/tabs/tab2');
+    switch (registro.type){
+      case 'http':
+
+        break;
+    }
 
   }
 }
